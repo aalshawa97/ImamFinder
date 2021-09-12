5fbf75f9136c58010510f6996fc35d750b8123df
aalshawa97 <abdullahalshawa@gmail.com>
2021-09-11 17:55:26 -0700

    Image chooser

--- app/src/main/java/com/example/detailapplication/activity/LoginActivity.kt
+++ app/src/main/java/com/example/detailapplication/activity/LoginActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
+import android.widget.*
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import android.widget.Toast
 import com.example.detailapplication.R
 import com.google.android.gms.tasks.Task
 
@@ -21,10 +22,24 @@ import com.google.android.gms.common.api.ApiException
 
 //@repeatable
 class LoginActivity : AppCompatActivity(){
+    lateinit var etName: EditText   //declartion
+    lateinit var tvRes: TextView
+    lateinit var IVPreviewImage: ImageView
+    private var imageUri: Uri? = null
+
+    // One Button
+    var BSelectImage: Button? = null
+    val pickImage = 100
+    // constant to compare
+    // the activity result code
+    var SELECT_PICTURE = 200
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            IVPreviewImage.setImageURI(imageUri)
+        }
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         /*
         val RC_SIGN_IN
@@ -45,6 +60,16 @@ class LoginActivity : AppCompatActivity(){
         lateinit var btnSignIn: Button
         super.onCreate(savedInstanceState)
         setContentView(com.example.detailapplication.R.layout.activity_login)
+        BSelectImage = findViewById(R.id.BSelectImage)
+        IVPreviewImage = findViewById(R.id.imageView)
+        etName = findViewById(R.id.etName) //initialization
+        tvRes = findViewById(R.id.tvResult)
+        title = "ImamFinder"
+        BSelectImage?.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+        Log.d("LoginActivity", "Hello World")
         //btnSignIn = findViewById(R.id.btnSignIn)
         //viewInitializations()
         //Configure Google sign in
@@ -52,7 +77,7 @@ class LoginActivity : AppCompatActivity(){
     }
 
     fun onClick(v: View) {
-        Toast.makeText(this,"Welcome user!", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Welcome " + etName.toString(), Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_item_detail)
         /*
         when (v.getId()) {
@@ -60,6 +85,22 @@ class LoginActivity : AppCompatActivity(){
         }
         */
     }
+
+    // this function is triggered when
+    // the Select Image Button is clicked
+    fun imageChooser(view: android.view.View) {
+
+        // create an instance of the
+        // intent of the type image
+        val i = Intent()
+        i.type = "image/*"
+        i.action = Intent.ACTION_GET_CONTENT
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE)
+    }
+
 
     private fun signIn() {
         // Build a GoogleSignInClient with the options specified by gso.
