0c9706f1f2cdd6fe7c661b745a6fa3dd5a15575a
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-01 15:54:43 -0700

    Imam is entity

--- app/src/main/java/com/example/detailapplication/Imam.java
+++ app/src/main/java/com/example/detailapplication/Imam.java
@@ -1,8 +1,15 @@
 package com.example.detailapplication;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Imam {
 
+    @ColumnInfo
     private String name;
+    @PrimaryKey(autoGenerate=true)
     private String number;
 
     public Imam(String name, String number) {
