29afd6d5a37b382525a7c7006450f576023dd761
aalshawa97 <abdullahalshawa@gmail.com>
2021-10-01 13:08:30 -0700

    Unit test

--- app/src/androidTest/java/com/example/detailapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/detailapplication/ExampleInstrumentedTest.kt
@@ -21,4 +21,11 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.detailapplication", appContext.packageName)
     }
+
+    @Test
+    fun useNotAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertNotEquals("Random", appContext.packageName)
+    }
 }
