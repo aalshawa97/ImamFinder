254ae1ff7d6dcc629242e87017e79402e6a7bff2
aalshawa97 <abdullahalshawa@gmail.com>
2022-01-14 16:45:27 -0800

    Chatbot and user images!

---
+++ app/src/main/java/com/example/detailapplication/ChatsModel.java
@@ -0,0 +1,28 @@
+package com.example.detailapplication;
+
+public class ChatsModel {
+
+    private String message;
+    private String sender;
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getSender(){
+        return sender;
+    }
+
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+
+    public ChatsModel(String message) {
+        this.message = message;
+        this.sender = sender;
+    }
+}
