062aa88e7737bb8990667f1a49e261b32fdd29ef
aalshawa97 <abdullahalshawa@gmail.com>
2022-01-21 05:25:25 -0800

    Clean up

--- app/src/main/java/com/example/detailapplication/activity/HomeActivity.kt
+++ app/src/main/java/com/example/detailapplication/activity/HomeActivity.kt
@@ -24,16 +24,6 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
-        findViewById<RecyclerView>(R.id.list).apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = MainAdapter(context, mutableListOf(
-                Mask, NinePatchMask, RoundedCorners, CropTop, CropCenter, CropBottom, CropSquare, CropCircle,
-                CropCircleWithBorder, Grayscale, BlurLight, BlurDeep, Toon, Sepia, Contrast, Invert,
-                Pixel, Sketch, Swirl, Brightness, Kuawahara, Vignette
-            ))
-        }
-        */
         setContentView(com.example.detailapplication.R.layout.activity_imam_finder_page)
 
         Timer("SettingUp", false).schedule(3000) {
